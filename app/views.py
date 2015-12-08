bbe6f0eb8e0bf4111aef4b0d8db6a889be9999b9
Thomas Young <cumtys@gmail.com>
2015-12-08 12:37:34 +0800

    加入了模板

--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "this is my microblog"
+    user = {'nickname':'Thomas Young'}
+    return render_template("index.html",
+                           title = 'Heme',
+                           user = user)
